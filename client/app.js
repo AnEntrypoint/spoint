37e692697dd1c75020e063c62389f6954920f668
System <system@local>
2026-02-08 23:53:14 +0200

    fix: Reduce shadow map to 512

--- client/app.js
+++ client/app.js
@@ -28,7 +28,7 @@ scene.add(studio)
 const sun = new THREE.DirectionalLight(0xffffff, 1.6)
 sun.position.set(30, 50, 20)
 sun.castShadow = true
-sun.shadow.mapSize.set(2048, 2048)
+sun.shadow.mapSize.set(512, 512)
 sun.shadow.bias = 0.0
 sun.shadow.normalBias = 0.0
 sun.shadow.camera.near = 1
