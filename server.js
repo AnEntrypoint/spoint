bd573d35e30662d227365ffd509d0cd4409f5c10
User <user@example.com>
2026-02-22 09:22:55 +0200

    feat: add scaffold command and SKILL.md for skills npm package

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 import { boot } from './src/sdk/server.js'
-await boot()
+import { scaffold } from './src/sdk/scaffold.js'
+
+const cmd = process.argv[2]
+if (cmd === 'scaffold') {
+  await scaffold()
+} else {
+  await boot()
+}
